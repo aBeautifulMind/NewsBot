f36cb96aaeb92ee0dcdd31bab4a9b2685eccb427
EmanueleGhelfi <manughelfi1994@gmail.com>
2016-04-05 23:32:28 +0200

    - Added some library

--- src/HTMLParsing/HDBlogParser.java
+++ src/HTMLParsing/HDBlogParser.java
@@ -1,12 +1,42 @@
 package HTMLParsing;
 
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 /**
  * Created by Emanuele on 05/04/2016.
  */
 public class HDBlogParser extends BaseParser {
     private final String url = "http://www.hdblog.it/";
+    private String selector=".newlist_normal";
+    private String lastTitle;
+
+    public HDBlogParser(String selector) {
+        this.selector = selector;
+    }
+
+    public HDBlogParser() {
+    }
 
     public void ParsePage() {
+        try {
+            Document doc = getDocument(url);
+            Elements elements = doc.select(selector);
+            System.out.println("elements: "+elements.size());
+            for (Element element: elements) {
+                System.out.println(element);
+            }
+            System.out.println(elements);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
+    //test
+    public static void main(String[] args){
+        System.out.println("Init...");
+        HDBlogParser hdBlogParser = new HDBlogParser();
+        hdBlogParser.ParsePage();
     }
 }
